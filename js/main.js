ce346d2d0494a98e8e5e96faea0e20af9b57f8f5
Vũ nè <thien.vu97ht123@gmail.com>
2022-12-12 22:37:31 +0700

    219. Integrate game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-import { GAME_STATUS } from './constants'
+import { GAME_STATUS, PAIRS_COUNT } from './constants.js'
+import { getColorElementList, getColorListElement, getInActiveColorList } from './selectors.js'
+import { getRandomColorPairs } from './utils.js'
 
 // Global variables
 let selections = []
-let gameState = GAME_STATUS.PLAYING
+let gameStatus = GAME_STATUS.PLAYING
 
 // TODOs
 // 1. Generating colors using https://github.com/davidmerfield/randomColor
@@ -10,3 +12,84 @@ let gameState = GAME_STATUS.PLAYING
 // 3. Check win logic
 // 4. Add timer
 // 5. Handle replay click
+
+// handleClick 1
+// handleClick 2
+// handleClick 3
+// setTimeout 2 --> reset selections
+// setTimeout 3 --> errors here
+function handleColorClick(liElement) {
+  const shouldBlockClick = [GAME_STATUS.BLOCKING, GAME_STATUS.FINISHED].includes(gameStatus)
+  if (!liElement || shouldBlockClick) return
+
+  // show color for clicked cell
+  liElement.classList.add('active')
+
+  // save clicked cell to selections
+  selections.push(liElement)
+  if (selections.length < 2) return
+
+  // check match
+  const firstColor = selections[0].dataset.color
+  const secondColor = selections[1].dataset.color
+  const isMatch = firstColor === secondColor
+
+  if (isMatch) {
+    // check win
+    const isWin = getInActiveColorList().length === 0
+    if (isWin) {
+      // show replay
+      // show YOU WIN
+    }
+
+    selections = []
+
+    return
+  }
+
+  // in case of not match
+  // remove active class for 2 li element
+  gameStatus = GAME_STATUS.BLOCKING
+
+  setTimeout(() => {
+    selections[0].classList.remove('active')
+    selections[1].classList.remove('active')
+
+    // reset selections for the next selections
+    selections = []
+
+    gameStatus = GAME_STATUS.PLAYING
+  }, 500)
+}
+
+function initColors() {
+  // random 8 pairs of colors
+  const colorList = getRandomColorPairs(PAIRS_COUNT)
+
+  // bind to li > div.overlay
+  const liList = getColorElementList()
+  liList.forEach((liElement, index) => {
+    liElement.dataset.color = colorList[index]
+
+    const overlayElement = liElement.querySelector('.overlay')
+    if (overlayElement) overlayElement.style.backgroundColor = colorList[index]
+  })
+}
+
+function attachEventForColorList() {
+  const ulElement = getColorListElement()
+  if (!ulElement) return
+
+  ulElement.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'LI') return
+
+    handleColorClick(event.target)
+  })
+}
+
+// main
+;(() => {
+  initColors()
+
+  attachEventForColorList()
+})()
